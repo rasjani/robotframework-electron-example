6f6e9990c8302fb754b4fefcec93844a38b35609
Jani Mikkonen <jani.mikkonen@gmail.com>
2024-05-08 13:49:04 +0300

    updated vars.py

--- vars.py
+++ vars.py
@@ -6,8 +6,8 @@ from selenium.webdriver import ChromeService
 # create signal_service and point it to correct version of preinstalled chromedriver
 # version should match your electron framework and those can be downloaded from:
 # https://github.com/electron/electron/releases under the version, from assets.
-signal_service = ChromeService(executable_path=r"/Users/rasjani/src/omat/electron-test/venv/bin/chromedriver")
+signal_service = ChromeService(executable_path=r"/full/path/to/your/chromedriver/location/chromedriver")
 
 # And here we construct options that point to use your actual electron app.
 signal_electron = Options()
-signal_electron.binary_location = r"/Applications/Signal.app/Contents/MacOS/Signal"
+signal_electron.binary_location = r"/full/path/to/your/electron/app/YourAppBinaryHere"
